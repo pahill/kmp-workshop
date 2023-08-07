6ce6bc869d5971c606e4e11ace6294338595142b
pahill <pamela.a.hill@gmail.com>
2023-08-07 14:15:21 +0200

    Step 6 - starter

---
+++ step6-testing/starter/weather-app/shared/build.gradle.kts
@@ -0,0 +1,137 @@
+import org.jetbrains.compose.internal.utils.getLocalProperty
+import java.util.*
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+    kotlin("plugin.serialization") version "1.9.0"
+    id("com.codingfeline.buildkonfig") version "0.13.3"
+}
+
+kotlin {
+    androidTarget()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64() {
+        // TODO: remove after 1.5 release
+        binaries.forEach {
+            it.freeCompilerArgs += listOf(
+                "-linker-option", "-framework", "-linker-option", "Metal",
+                "-linker-option", "-framework", "-linker-option", "CoreText",
+                "-linker-option", "-framework", "-linker-option", "CoreGraphics",
+            )
+        }
+    }
+
+
+    cocoapods {
+        version = "1.0.0"
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        ios.deploymentTarget = "14.1"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "shared"
+            isStatic = true
+        }
+        extraSpecAttributes["resources"] =
+            "['src/commonMain/resources/**', 'src/iosMain/resources/**']"
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+                api("io.github.qdsfdhvh:image-loader:1.6.3")
+                implementation("cafe.adriel.voyager:voyager-navigator:1.0.0-rc05")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("io.ktor:ktor-client-core:2.3.3")
+                implementation("io.ktor:ktor-client-content-negotiation:2.3.3")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                //implementation(kotlin("test-common"))
+                //implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                api("androidx.activity:activity-compose:1.7.2")
+                api("androidx.appcompat:appcompat:1.6.1")
+                api("androidx.core:core-ktx:1.10.1")
+                implementation("io.ktor:ktor-client-android:2.3.3")
+            }
+        }
+        val androidUnitTest by getting {
+            dependencies {
+                //implementation(kotlin("test-junit"))
+                //implementation("junit:junit:4.13.2")
+            }
+        }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.3")
+            }
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
+}
+
+android {
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+    namespace = "com.myapplication.common"
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+        targetSdk = (findProperty("android.targetSdk") as String).toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        jvmToolchain(11)
+    }
+}
+
+buildkonfig {
+    packageName = "com.myapplication"
+    objectName = "ApiKeyConfig"
+    exposeObjectWithName = "Config"
+
+    defaultConfigs {
+        buildConfigField(
+            com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING,
+            "WeatherApiKey",
+            getLocalProperty("WEATHER_API_KEY").toString()
+        )
+    }
+}
---
+++ step6-testing/starter/weather-app/shared/src/commonTest/kotlin/CelsiusToFahrenheitTest.kt
@@ -0,0 +1,3 @@
+class CelsiusToFahrenheitTest {
+    //TODO: 3 tests
+}
