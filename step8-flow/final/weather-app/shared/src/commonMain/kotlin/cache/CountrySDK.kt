66bc394cf42768fe6386ce5041f24d1376d8979e
pahill <pamela.a.hill@gmail.com>
2023-08-08 14:10:01 +0200

    Step 8 - final

---
+++ step8-flow/final/weather-app/shared/src/commonMain/kotlin/cache/CountrySDK.kt
@@ -0,0 +1,22 @@
+package cache
+
+import country.Country
+import io.github.xxfast.kstore.KStore
+import io.github.xxfast.kstore.file.storeOf
+import network.CountryApi
+
+class CountrySDK {
+    private val cache: KStore<List<Country>> = storeOf(filePath = pathToCountryCache())
+    private val api: CountryApi = CountryApi()
+
+    @Throws(Exception::class)
+    suspend fun getLaunches(): List<Country> {
+        return cache.get()
+            ?: api.getAllCountries().also {
+                cache.delete()
+                cache.set(it)
+            }
+    }
+}
+
+expect fun pathToCountryCache(): String
