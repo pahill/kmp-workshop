2d2e02c3266ca347a2d853bf9f534abffe1d27e2
pahill <pamela.a.hill@gmail.com>
2023-08-08 14:14:17 +0200

    Step 8 - starter

---
+++ step8-flow/starter/weather-app/shared/src/commonMain/kotlin/time/TimeApi.kt
@@ -0,0 +1,24 @@
+package time
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Duration.Companion.seconds
+
+class TimeApi {
+    private val clockTime = TODO()
+
+    val actualTime = clockTime.stateIn(GlobalScope, SharingStarted.Eagerly, "N/A")
+
+    private fun formatTime(time: Instant): String {
+        val localDateTime = time.toLocalDateTime(TimeZone.currentSystemDefault())
+        return "${localDateTime.hour}:${localDateTime.minute}:${localDateTime.second}"
+    }
+
+}
